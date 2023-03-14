ac78a37a7b2c1b457d5aef38db9f15ebeb8d1e92
Uiro22 <125377728+Uiro22@users.noreply.github.com>
2023-03-14 14:32:13 +0900

    Update memo.rb

--- MEMO/memo.rb
+++ MEMO/memo.rb
@@ -1,30 +1,33 @@
 require "csv" # CSVファイルを扱うためのライブラリを読み込んでいます
 
 puts "1 → 新規でメモを作成する / 2 → 既存のメモを編集する"
-
-memo_type = gets.to_s # ユーザーの入力値を取得し、数字へ変換しています
-input_number = memo_type.chomp
+memo_type = gets.to_i # ユーザーの入力値を取得し、数字へ変換しています
 
 # if文を使用して続きを作成していきましょう。
-if input_number == "1"
-     puts "拡張子を除いたファイル名を入力してください。"
-     file_name = gets.chomp
+if memo_type == 1
+    puts "拡張子を除いたファイル名を入力してください。"
+    file_name = gets.chomp
      
-     puts "メモしたい内容を記入してください。"
-     puts "完了したらCtrl＋Dを押します"
-     input_memo = STDIN.read
-     memo = input_memo.chomp
+    puts "メモしたい内容を記入してください。"
+    puts "完了したらCtrl＋Dを押します"
+    memo_type = STDIN.read
+    memo = memo_type.chomp
 
-     CSV.open("#{file_name}.csv", "w") do |csv|
-     csv << [memo]
+    CSV.open("#{file_name}.csv", "w") do |csv|
+    csv << [memo]
 end
 
-elsif input_number == "2"
-     puts "拡張子を除いた既存ファイル名を入力してください。"
-     file_name = gets.chomp
-     
-     puts "メモの内容です。"
-     memo_list = []
+elsif memo_type == 2
+    puts "拡張子を除いた既存ファイル名を入力してください。"
+    file_name = gets.chomp
+
+    puts "メモしたい内容を記入してください。"
+    puts "完了したらCtrl＋Dを押します"
+    memo_type = STDIN.read
+    memo = memo_type.chomp
+    
+    puts "メモの内容です。"
+    memo_list = []
     CSV.foreach("#{file_name}.csv") do |row|
     memo_list << row[0]
     puts "#{memo_list.length}. #{row[0]}"
@@ -34,11 +37,6 @@ elsif input_number == "2"
     CSV.foreach("#{file_name}.csv") do |row|
     memo = row[0]
     end
-
-    puts "メモしたい内容を記入してください。"
-    puts "完了したらCtrl＋Dを押します"
-    input_memo = STDIN.read
-    memo = input_memo.chomp
 
     CSV.open("#{file_name}.csv", "a") do |csv|
     memo_list.pop
